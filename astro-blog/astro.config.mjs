72d62455f0e3773815db1867208f6814e649826e
jp-knj <kenji.tomita1996@gmail.com>
2022-04-04 22:30:23 +0900

    :construction: install tailwind css

--- astro-blog/astro.config.mjs
+++ astro-blog/astro.config.mjs
@@ -1,3 +1,8 @@
 import { defineConfig } from 'astro/config';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    devOptions: {
+        tailwindConfig: './tailwind.config.js',
+    },
+});
